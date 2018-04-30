c4688fb70b36d9363a89a70a4a591dc828234323
Greg Lee <greg.lee1@pcc.edu>
2018-04-29 21:23:07 -0700

    added goodbye world to project

--- src/hello_world/HelloWorld.java
+++ src/hello_world/HelloWorld.java
@@ -2,10 +2,12 @@ package hello_world;
 /**
  * Author: Greg Lee
  *
+ * added goodbye world to program
  */
 public class HelloWorld {
     public static void main(String[] args) {
         // Prints "Hello, World" in the terminal window.
         System.out.println("Hello, World");
+        System.out.println("Goodbye World");
     }
 }
